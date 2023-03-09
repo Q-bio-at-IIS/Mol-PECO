51a243c05124cc1c525721566e80654e65ddac21
zmj <mengji.zhang0809@gmail.com>
2023-03-09 11:51:07 +0800

    update the norm of CM for LPE and weighted AM
    mat-wise norm for weighted AM; row-wise norm for LPE

--- codes/preprocess_atom2id.py
+++ codes/preprocess_atom2id.py
@@ -88,12 +88,8 @@ class Preprocessor(object):
         bin_arr = bin_arr.astype("int")
         return bin_arr
 
-    def _norm_coulomb(self, data, mol_size, epsilon = 1e-9): ## normalization in matrix-wise
-        # data = [[0,2,3],[2,0,4],[3,4,0]]
-        data = np.array(data)
-        ## set the diagnoal to 0
-        diag = np.diag(np.diag(data))
-        data -= diag
+    def _norm_coulomb_mat(self, data, mol_size, epsilon = 1e-9): ## normalization in matrix-wise
+        # print(data[:mol_size, :mol_size])
         ## perform normalization
         if self.cou_norm == "minmax":
             min_ = np.min(data, keepdims = True)
@@ -111,32 +107,68 @@ class Preprocessor(object):
         if ~self.check_symmetric(new_data["coulomb"][:mol_size,:mol_size]):
             print(new_data["coulomb"][:mol_size,:mol_size])
             print("\n\n\n")
+        # print(new_data["coulomb"][:mol_size,:mol_size])
+        # print("=="*20)
         return new_data
+
+    def _norm_coulomb_row(self, data, mol_size, epsilon = 1e-9): ## z-score
+        # print(data[:mol_size, :mol_size])
+        if self.cou_norm == "zscore":
+            mu = np.mean(data, axis = -1, keepdims = True)
+            sigma = np.std(data, axis = -1, keepdims = True)
+            new_data = {"coulomb": (data-mu)/(sigma + epsilon)}
+        elif self.cou_norm == "minmax":
+            min_ = np.min(data, axis = -1, keepdims = True)
+            max_ = np.max(data, axis = -1, keepdims = True)
+            new_data = {"coulomb": (data - min_)/(max_ - min_ + epsilon)}
+        elif self.cou_norm == "frobenius":
+            norm = np.linalg.norm(data, axis = -1, keepdims = True)
+            new_data = {"coulomb": data/(norm + epsilon)}
+        elif self.cou_norm == "binary":
+            new_data = {"coulomb": data}
+            new_data["coulomb0"] = self._binarilize(self._sigmoid(data))
+            for s in range(1, self.step_binary):
+                new_data["coulomb{}".format(s)] = self._binarilize(self._sigmoid(data + s))
+                new_data["coulomb{}".format(-s)] = self._binarilize(self._sigmoid(data - s))
+        elif self.cou_norm == "none":
+            new_data = {"coulomb": data}
+        else:
+            raise("normalization of coulomb is {}, which is not implemented".format(self.method))
+        # print(new_data["coulomb"][:mol_size,:mol_size])
+        # print("=="*20)
+        return new_data["coulomb"]
+
+    def check_symmetric(self, a, tol=1e-8):
+        return np.all(np.abs(a-a.T) < tol)
 
     def _get_features(self, mol):
         if not self._check_num_atoms(mol, self.max_atoms):
-            return None, None, None, mol.GetNumAtoms(), False
+            return None, None, None, mol.GetNumAtoms(), None, False
 
         atom_array = self._construct_atomic_number_array(mol, self.max_size)
         adj_array = self._construct_discrete_edge_matrix(mol, self.max_size)
         coulomb_array, status = cal_coulomb_matrix(mol, self.max_size)
-        if status:
-            coulomb_array = self._norm_coulomb(coulomb_array)
         mol_size = mol.GetNumAtoms()
-        return atom_array, adj_array, coulomb_array, mol_size, status
+        weighted_am, coulomb_lpe = None, None
+        if status:
+            weighted_am = self._norm_coulomb_mat(coulomb_array, mol_size)
+            coulomb_lpe = self._norm_coulomb_row(coulomb_array, mol_size)
+        
+        return atom_array, adj_array, weighted_am, mol_size, coulomb_lpe, status
 
     def process(self, smiles):
         mol, canonical_smiles, status = self._prepare_mol(smiles)
         if status:
-            atom_array, adj_array, coulomb_array, mol_size, flag = self._get_features(mol)
-            return atom_array, adj_array, coulomb_array, mol_size, canonical_smiles, flag
+            atom_array, adj_array, weighted_am, mol_size, coulomb_lpe, flag = self._get_features(mol)
+            return atom_array, adj_array, weighted_am, mol_size, canonical_smiles, coulomb_lpe, flag
         else:
-            return None, None, None, None, None, False
+            return None, None, None, None, None, None, False
 
 class DataDumperLoader(object):
     """docstring for ClassName"""
     def __init__(self, data_dir, cou_norm = "frobenius", max_atoms = 28, max_size = 28, atom2id_path = "", laplace = False, freq = 10, add_Hs = False, self_loop = False, step_binary = 10):
         super(DataDumperLoader, self).__init__()
+        os.makedirs(data_dir, exist_ok = True)
         self.max_atoms = max_atoms
         self.max_size = max_size
         self.data_dir = data_dir
@@ -163,8 +195,8 @@ class DataDumperLoader(object):
         EigVals, EigVecs = laplace_decomp(A, max_freqs = self.freq)
         return EigVals.numpy(), EigVecs.numpy()
 
-    def _cal_laplace_coulomb(self, coulomb):
-        # print(coulomb.shape)
+    def _cal_laplace_coulomb(self, coulomb, mol_size):
+        # print(coulomb[:mol_size, :mol_size])
         EigVals, EigVecs = laplace_decomp(coulomb, max_freqs = self.freq)
         return EigVals.numpy(), EigVecs.numpy()
 
@@ -172,7 +204,7 @@ class DataDumperLoader(object):
         self.atom_dict, self.adj_dict, self.coulomb_dict, self.mol_masks, self.mol_sizes = {}, {}, {}, {}, {}
         self.eigVal_adj, self.eigVec_adj, self.eigVal_coulomb, self.eigVec_coulomb = {}, {}, {}, {}
         for smiles in tqdm(self.smiles_list, ncols = 80):
-            atom_array, adj_array, coulomb_array, mol_size, canonical_smiles, flag = self.processor.process(smiles)
+            atom_array, adj_array, weighted_am, mol_size, canonical_smiles, coulomb_lpe, flag = self.processor.process(smiles)
 
             if not flag:
                 continue
@@ -182,16 +214,18 @@ class DataDumperLoader(object):
                 self.adj_dict[smiles] = adj_array 
                 self.mol_masks[smiles] = self._build_mol_mask(mol_size)
                 self.mol_sizes[smiles] = mol_size
-                self.coulomb_dict[smiles] = coulomb_array
+                self.coulomb_dict[smiles] = weighted_am
                 self.eigVal_adj[smiles], self.eigVec_adj[smiles] = self._cal_laplace_adj(adj_array)
-                self.eigVal_coulomb[smiles], self.eigVec_coulomb[smiles] = self._cal_laplace_coulomb(coulomb_array["coulomb"])
+                self.eigVal_coulomb[smiles], self.eigVec_coulomb[smiles] = self._cal_laplace_coulomb(coulomb_lpe, mol_size)
             else:
                 print("{} not in atom2id".format(smiles))
-
-    def _process_example(self):
-        smiles = "C1CCN(CC1)C(=O)C=CC=CC2=CC3=C(C=C2)OCO3"
+            # break
+            
+    def _process_example(self, smiles = "C1CCN(CC1)C(=O)C=CC=CC2=CC3=C(C=C2)OCO3"):
         atom_array, adj_array, coulomb_array, mol_size, canonical_smiles, flag = self.processor.process(smiles)
-        return atom_array, adj_array, coulomb_array, mol_size, canonical_smiles, flag
+        eigVal_adj, eigVec_adj = self._cal_laplace_adj(adj_array)
+        eigVal_coulomb, eigVec_coulomb = self._cal_laplace_coulomb(coulomb_array["coulomb"])
+        return atom_array, adj_array, coulomb_array, mol_size, canonical_smiles, eigVal_adj, eigVec_adj, eigVal_coulomb, eigVec_coulomb
 
     def _build_atom2id(self, atom_array):
         ids, status = [], True
